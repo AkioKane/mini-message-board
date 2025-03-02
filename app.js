92da1036c06b64c19cf23b2079cb1e2d26011acd
Akio Kane <aakocherga@mail.ru>
2025-03-02 22:52:22 +0700

    Added new message page

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const path = require("path");
+
+const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/newMessageRouter");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Home")
-})
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/new", newMessageRouter);
+app.use("/", indexRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
